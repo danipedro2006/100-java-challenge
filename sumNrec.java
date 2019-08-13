0f44b92def2b5441ffab679969355988b260560f
danipedro2006 <danipedro2006@yahoo.com>
2019-08-13 10:17:55 +0300

    first commit

---
+++ sumNrec.java
@@ -0,0 +1,22 @@
+package javacamp;
+
+public class sumNrec {
+
+	public static void main(String[] args) {
+		int[] n= {1,2,3,4,5,6,7,8,9,10};
+		int i=n.length-1;
+		System.out.println(sum(n,0));
+	}
+
+	private static int sum(int[] n, int i) {
+		if(i<1)
+			{
+				return n[i];
+			}
+		else
+			{
+				return n[i]+sum(n,i-1);
+			}
+	}
+
+}
---
+++ swapw.java
@@ -0,0 +1,15 @@
+package javacamp;
+
+public class swapw {
+
+	public static void main(String[] args) {
+		int a=2, b=3, c=4;
+		a=b^2;
+		b=a^1;
+		a=a^2;
+		
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
